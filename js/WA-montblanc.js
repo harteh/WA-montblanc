08f9e61aa378d092031cda136d7e380dd58a08c6
harteh <harteh31@gmail.com>
2019-12-26 17:20:27 +0900

    스크립트 적용 끝

--- js/WA-montblanc.js
+++ js/WA-montblanc.js
@@ -1,10 +1,10 @@
-$("document").ready(function(){
-    
+$("document").ready(function(){   
+
     var menu = $("header .menu"),        
         nav = $("#dep1"),
         nav_li = $("#dep1 ul li"),
         subMenu = $("#dep2 ul"),
-        subMenu_li = $("#dep2 ul li");
+        i = 0;
 
     $(menu).click(function(){
         // 메뉴 버튼 클릭 시 네비게이션(메뉴)이 보여지고
@@ -13,20 +13,26 @@ $("document").ready(function(){
         menu.toggleClass("close")
     });
     
-    $(nav_li).hover(function(){
-        // 네비게이션에 호버 되면 서브 메뉴가 보여짐
-        var i = $(this).index();
+    nav_li.hover(function(){
+        // 네비게이션(주 메뉴)에 호버 되면 서브 메뉴가 보여짐
+        i = $(this).index();
 
         $(this).find("a").css("textShadow", "#cecece 10px 0 10px")
         $("#dep2").show()
         subMenu.eq(i).show()
-        
+
     }, function(){
         // 호버가 빠지면 서브 메뉴가 사라짐
         $(this).find("a").css("textShadow", "")
-        $("#dep2").hide()
         subMenu.hide()
     });     // 하위 메뉴 설정 끝
+
+    $("#dep2").hover(function(){
+        subMenu.eq(i).show()
+        $("#dep").show();
+    }, function(){
+        subMenu.hide()
+    });     // 서브 메뉴에 호버시에도 메뉴 유지하게 설정 끝
 
 
     var swiper = new Swiper('#disney .swiper-container', {
