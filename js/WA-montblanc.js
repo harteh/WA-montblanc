c70e6c4c5838b3718f24db6e5f1f38fa207d1da9
harteh <harteh31@gmail.com>
2019-12-26 12:57:37 +0900

    토글메뉴작성중

--- js/WA-montblanc.js
+++ js/WA-montblanc.js
@@ -5,8 +5,28 @@ $("document").ready(function(){
         var current = ($(window).scrollTop() / ($(document).outerHeight() - $(window).height())) * 100;
         // outerHeight = 보더 값을 포함한 세로길이 값
         $(".disney-indicator").width(current + "%")
-    })
-    // 디즈니 슬라이드 인디게이터 영역  끝
+    });
+    // 디즈니 슬라이드 인디게이터 설정  끝
 
+    var menu = $("header .menu ul"),
+        nav = $("#dep1"),
+        nav_li = $("#dep1 ul li"),
+        subMenu = $("#dep2 ul");
+
+    $(menu).click(function(){
+        // 메뉴 버튼 클릭 시 네비게이션 이 보여짐
+        nav.toggle()        
+    });
+    
+    $(nav_li).hover(function(){
+        // 네비게이션에 호버 되면 서브 메뉴가 보여짐
+        var i = $(this).index();
+        $("#dep2").show()
+        subMenu.eq(i).show()
+    }, function(){
+        // 호버가 빠지면 서브 메뉴가 사라짐
+        $("#dep2").hide()
+        subMenu.hide()
+    });     // 하위 메뉴 설정 끝
 
 })
