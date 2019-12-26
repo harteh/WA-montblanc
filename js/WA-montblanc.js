797898286c6aabadc3e15459c86de636f343e74c
harteh <harteh31@gmail.com>
2019-12-26 17:45:57 +0900

    dep2 오류 수정

--- js/WA-montblanc.js
+++ js/WA-montblanc.js
@@ -3,10 +3,11 @@ $("document").ready(function(){
     var menu = $("header .menu"),        
         nav = $("#dep1"),
         nav_li = $("#dep1 ul li"),
+        sub = $("#dep2"),
         subMenu = $("#dep2 ul"),
-        i = 0;
+        u = 0;
 
-    $(menu).click(function(){
+    menu.click(function(){
         // 메뉴 버튼 클릭 시 네비게이션(메뉴)이 보여지고
         //  메뉴 버튼이 변경 됨
         nav.toggle()
@@ -15,21 +16,21 @@ $("document").ready(function(){
     
     nav_li.hover(function(){
         // 네비게이션(주 메뉴)에 호버 되면 서브 메뉴가 보여짐
-        i = $(this).index();
+        u = $(this).index();
 
         $(this).find("a").css("textShadow", "#cecece 10px 0 10px")
-        $("#dep2").show()
-        subMenu.eq(i).show()
-
+        sub.show()
+        subMenu.eq(u).show()
     }, function(){
         // 호버가 빠지면 서브 메뉴가 사라짐
         $(this).find("a").css("textShadow", "")
+        sub.hide()
         subMenu.hide()
     });     // 하위 메뉴 설정 끝
 
-    $("#dep2").hover(function(){
-        subMenu.eq(i).show()
-        $("#dep").show();
+    sub.hover(function(){
+        subMenu.eq(u).show()
+        sub.show()
     }, function(){
         subMenu.hide()
     });     // 서브 메뉴에 호버시에도 메뉴 유지하게 설정 끝
