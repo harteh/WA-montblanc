4cc7842bb4f718a856dee8f99ed0c137cbdf3ed9
harteh <harteh31@gmail.com>
2019-12-26 16:42:48 +0900

    메뉴 토글 작성

--- js/WA-montblanc.js
+++ js/WA-montblanc.js
@@ -1,26 +1,33 @@
 $("document").ready(function(){
     
-    var menu = $("header .menu ul"),
+    var menu = $("header .menu"),        
         nav = $("#dep1"),
         nav_li = $("#dep1 ul li"),
-        subMenu = $("#dep2 ul");
+        subMenu = $("#dep2 ul"),
+        subMenu_li = $("#dep2 ul li");
 
     $(menu).click(function(){
-        // 메뉴 버튼 클릭 시 네비게이션 이 보여짐
-        nav.toggle()        
+        // 메뉴 버튼 클릭 시 네비게이션(메뉴)이 보여지고
+        //  메뉴 버튼이 변경 됨
+        nav.toggle()
+        menu.toggleClass("close")
     });
     
     $(nav_li).hover(function(){
         // 네비게이션에 호버 되면 서브 메뉴가 보여짐
         var i = $(this).index();
+
+        $(this).find("a").css("textShadow", "#cecece 10px 0 10px")
         $("#dep2").show()
         subMenu.eq(i).show()
+        
     }, function(){
         // 호버가 빠지면 서브 메뉴가 사라짐
+        $(this).find("a").css("textShadow", "")
         $("#dep2").hide()
         subMenu.hide()
     });     // 하위 메뉴 설정 끝
-    
+
 
     var swiper = new Swiper('#disney .swiper-container', {
         scrollbar: {
